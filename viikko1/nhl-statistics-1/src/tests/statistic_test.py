2c325cae6f8dfa0ce9199b18566d0562fb95c795
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-11-01 11:47:06 +0200

    exercise 16 done

---
+++ viikko1/nhl-statistics-1/src/tests/statistic_test.py
@@ -0,0 +1,38 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        self.statistics = Statistics(PlayerReaderStub())
+    
+    def test_search(self):
+        result = self.statistics.search("Yzerman")
+
+        self.assertEqual(result.name, "Yzerman")
+    
+    def test_search_wrong_name(self):
+        result = self.statistics.search("Stamkos")
+
+        self.assertEqual(result, None)
+
+    def test_team(self):
+        result = self.statistics.team("EDM")
+
+        self.assertEqual(result[0].name, "Semenko")
+    
+    def test_top(self):
+        result = self.statistics.top(3)
+        top3_names = [result[0].name, result[1].name, result[2].name]
+
+        self.assertEqual(top3_names, ["Gretzky", "Lemieux", "Yzerman"])
