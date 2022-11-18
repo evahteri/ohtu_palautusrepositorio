c37d29b73d06586284acac3e460fc0ae5aa73b2f
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-11-18 13:06:23 +0200

    week 4 exercise 2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataa_jos_ladattava_summa_on_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 5
+
+        self.kassa.lataa(maksukortti_mock, 5)
+
+        maksukortti_mock.lataa.assert_called_with(5)
+    
+    def test_ei_lataa_jos_ladattava_summa_on_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 5
+
+        self.kassa.lataa(maksukortti_mock, -12)
+
+        maksukortti_mock.lataa.assert_not_called()
