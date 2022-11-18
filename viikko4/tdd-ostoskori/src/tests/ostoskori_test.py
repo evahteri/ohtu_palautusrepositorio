6d7577acda75efaf1202b0ea685a13656799e5f1
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-11-18 17:35:22 +0200

    two same products added, the product has correct details

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -86,3 +86,12 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
 
         self.assertEqual(len(ostokset), 1)
+    
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikeat_tiedot(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual((ostokset[0].tuotteen_nimi(), ostokset[0].lukumaara()), ("Maito", 2))
