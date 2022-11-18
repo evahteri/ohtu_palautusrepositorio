7a75e7adb8ca200e1d12c668d53b20451f3a3456
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-11-18 15:16:30 +0200

    after adding two same products, the price is right

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -44,4 +44,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(),2)
+
+    def test_kahden_saman_tuotteen_lisäämisen_jälkeen_hinta_oikein(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(),6)
         
