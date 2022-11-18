f21aac241bcab060431145e1c32d80f89721b8a6
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-11-18 15:15:24 +0200

    after adding two same products, the amount is correct

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -37,3 +37,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(leipä)
 
         self.assertEqual(self.kori.hinta(), 8)
+    
+    def test_kahden_saman_tuotteen_lisäämisen_jälkeen_ostoskorissa_on_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.tavaroita_korissa(),2)
+        
