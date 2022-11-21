ef3ff71b79f9e214bc0a2d468511851851748174
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-11-21 10:07:10 +0200

    added test to make sure basket is empty after removing all products

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -105,3 +105,12 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
 
         self.assertEqual(ostokset[0].lukumaara(), 1)
+
+    def test_tuote_lisataan_ja_poistetaa_onko_ostoskori_tyhja(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual(len(ostokset), 0)
