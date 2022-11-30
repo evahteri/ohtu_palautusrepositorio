eee9390a9427853ae29c8193fa5402c654537c55
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-11-30 12:00:22 +0200

    week 6 exercise 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,69 +1,25 @@
 from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not, All, Or
-
+from query_builder import QueryBuilder 
 def main():
     url = "https://studies.cs.helsinki.fi//nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
-    )
+    query = QueryBuilder()
+    matcher = query.build()
+    print("all")
 
     for player in stats.matches(matcher):
         print(player)
     
-    matcher = And(
-    HasFewerThan(1, "goals"),
-    PlaysIn("NYR")
-    )
-    print("fewer than list:")
+    print("NYR")
+
+    matcher = query.playsIn("NYR").hasAtLeast(10, "goals").hasFewerThan(20, "goals").build()
 
     for player in stats.matches(matcher):
         print(player)
-
-    print("not list:")
-
-    matcher = And(
-    Not(HasAtLeast(1, "goals")),
-    PlaysIn("NYR")
-    )
-
-    for player in stats.matches(matcher):
-        print(player)
-    
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
-
-    matcher = Or(
-    HasAtLeast(45, "goals"),
-    HasAtLeast(70, "assists")
-    )
-
-    print("or-list:")
-
-    for player in stats.matches(matcher):
-        print(player)
-    
-    matcher = And(
-    HasAtLeast(70, "points"),
-    Or(
-        PlaysIn("NYR"),
-        PlaysIn("FLA"),
-        PlaysIn("BOS")
-    )
-    )
-
-
-    print("or-list 2:")
-
-    for player in stats.matches(matcher):
-        print(player)
-
-    
 
 
 if __name__ == "__main__":
