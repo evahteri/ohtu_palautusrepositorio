d394eac953ba21c84bc44fbbcd569cbc394b7203
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-11-30 11:16:36 +0200

    week 6 exercise 2

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,9 +1,9 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not, All
 
 def main():
-    url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+    url = "https://studies.cs.helsinki.fi//nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
@@ -15,6 +15,29 @@ def main():
 
     for player in stats.matches(matcher):
         print(player)
+    
+    matcher = And(
+    HasFewerThan(1, "goals"),
+    PlaysIn("NYR")
+    )
+    print("fewer than list:")
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("not list:")
+
+    matcher = And(
+    Not(HasAtLeast(1, "goals")),
+    PlaysIn("NYR")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+    
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+    
 
 
 if __name__ == "__main__":
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -15,6 +15,9 @@ class PlaysIn:
 
     def matches(self, player):
         return player.team == self._team
+    
+    def neg_matches(self, player):
+        return player.team != self._team
 
 class HasAtLeast:
     def __init__(self, value, attr):
@@ -25,3 +28,53 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+    def neg_matches(self, player):
+        player_value = getattr(player, self._attr)
+        return player_value < self._value
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+    
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+        return player_value < self._value
+    
+    def neg_matches(self, player):
+        player_value = getattr(player, self._attr)
+        return player_value >= self._value
+
+class DoesNotPlayIn:
+    def __init__(self, team):
+        self._team = team
+
+    def matches(self, player):
+        return player.team != self._team
+
+
+    def neg_matches(self, player):
+        return player.team == self._team
+
+class Not:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+    
+    def matches(self, player):
+        for matcher in self._matchers:
+            if not matcher.neg_matches(player):
+                return False
+        
+        return True
+
+class All:
+    def __init__(self):
+        pass
+
+    def matches(self, player):
+        return True
+
+
+
+
